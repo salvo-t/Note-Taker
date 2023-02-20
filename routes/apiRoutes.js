2442b6e15e13e94a7e309dade3f5c417d30be3f7
salvo-t <salvot.biz@gmail.com>
2023-02-19 21:32:28 -0500

    added routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+router.post('/notes', (req, res) => {
+
+  });
+
+  router.delete('/notes/:id', (req, res) => {
+
+  });
+
+  router.get('/notes', (req, res) => {
+
+  });
+
+  module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const publicRoutes = require('./routes/publicRoutes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001
@@ -6,5 +9,7 @@ const PORT = process.env.PORT || 3001
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use('/', publicRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
